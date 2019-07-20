209403eded69e34a2095750ad81449ee27d1e1e0
Konstantin Osipov <kostja@scylladb.com>
2019-07-20 21:49:23 +0300

    A lot of infrastructure up to TestFile::Run()
    Update README.md

--- README.md
+++ README.md
@@ -5,11 +5,84 @@ You need go 1.12 or later installed.
 
 To build:
 
-make; bin/yacht
+    $ make
+
+To use:
+
+Add a configuration file `~/.yacht.yaml`, an example configuration file
+is provided in the project directory.
+
+    $ ./yacht
+
+will create a temporary directory, initialize a Scylla server instance in it
+and run tests found in the source directory against this server. It will
+print output as the testing progress.
 
 
 What this program does
 ----------------------
 
-On start, it connects to an existing Cassandra instance, 
+On start, it connects to an existing Cassandra instance,
 creates 'yacht' keyspace, and on shutdown deletes the keyspace.
+
+
+## Definitions
+
+### Test suite
+
+A collection of tests in a single directory. The directory must provide a
+suite configuration file (suite.json or suite.yaml). A suite file
+contains suite description and type. The only supported type is "cql", which
+means that each .test.cql file in the suite is read linewise and sent to a
+Scylla server.
+
+### Test
+
+For CQL test suite, a single test file must have .test.cql extension.
+The harness creates an accompanying file with .result extension on the first
+test run. The .result file contains server output as produced by the tested
+Scylla server. If there
+is a result file, the harness will create a temporary file, store the server
+output in it, and compare it with the pre-recorded .result file when the
+test completes. The test is considered failed if executing any CQL statement
+produced an error (e.g.  because the server crashed during execution) or server
+output does not match one recorded in .result file. In the event of output
+mismatch a file testname.reject is created, and first lines of the diff
+between the two files are output. To update .result file with the new
+output, simply overwrite it with the reject file:
+    mv suitename/testname.re*
+
+A single CQL test file is a collection of test cases. Each test starts with
+-- yacht: test "test-case-name" line. The test case ends when the next test
+case is found or end-of-file marker is read. Using test cases within a large
+file allows to quickly navigate to a failed test, enable or disable
+individual test cases.
+
+Each test consists of files `*.test(.lua|.sql|.py)?`, `*.result`, and may have
+skip condition file `*.skipcond`.  On first run (without `.result`) `.result`
+is generated from output.  Each run, in the beggining, `.skipcond` file is
+executed. In the local env there's object `self`, that's `Test` object. If test
+must be skipped - you must put `self.skip = 1` in this file. Next,
+`.test(.lua|.py)?` is executed and file `.reject` is created, then `.reject` is
+compared with `.result`. If something differs, then 15 last string of this diff
+file are printed and `.reject` file is saving in the folder, where `.result`
+file is. If not, then `.reject` is deleted.
+
+### Lane
+
+Lane is a concept used to represent the runtime environment of a test. It
+includes a temporary directory with temporary artefacts, created during a
+test, such as server data and log file. The harness uses a short temporary
+name to identify a lane. When the harness is srarted, a new directory with
+lane name is created and a new server is initialized in this directory.
+When the testing ends successfully, the lane is cleaned up, and the
+directory is removed. Upon failure the lane directory is left intact. The
+lane is not only about a directory, but is a container of all external
+artefacts, such as used ports, running processes and so on. In future the
+harness will support multiple lanes, for parallel testing.
+
+### Server
+
+A server is the testing subject. It can be a standalone server, created
+outside the harness (`--uri` option) or a temporary instance created by the
+harness automatically.
--- yacht.go
+++ yacht.go
@@ -4,14 +4,52 @@ import "log"
 import "os"
 import "os/signal"
 import "fmt"
+import "bufio"
 
 import "path"
 import "path/filepath"
 import "strings"
 import "github.com/spf13/pflag"
 import "github.com/spf13/viper"
+import "github.com/gocql/gocql"
 
-//import "bufio"
+// A directory with tests
+type TestSuite interface {
+	FindTests(path string, patterns []string) error
+	IsEmpty() bool
+	PrepareLane(*Lane)
+	Run(force bool) error
+}
+
+// A single test
+type TestFile interface {
+	Run(force bool, c *Connection) error
+}
+
+// An artefact is anything left by a test or suite while
+// it runs. A keyspace created against a running server,
+// a runnign process, etc.
+// Some artefacts must be removed when shutting down,
+// others are cleared between tests.
+// Artefacts such as server data diretory or log file
+// are removed when a test starts, not when it ends, to
+// be able to inspect them in case of a crash or test failure.
+type Artefact interface {
+	Remove()
+}
+
+// A connection is used by a test file to execute queries
+type Connection interface {
+	Execute(query string) (string, error)
+	Close()
+}
+
+// A server is a server or instance and something we can connect to
+// and run queries
+type Server interface {
+	Start(lane *Lane) error
+	Connect() (Connection, error)
+}
 
 // Yacht running environment.
 type Env struct {
@@ -32,6 +70,8 @@ type Env struct {
 	builddir string
 }
 
+// Look up a configuration file and load it if found
+// Exit on error, such as incorrect configuration syntax.
 func (env *Env) configure() {
 	// A configuration file can be provided to help yacht find
 	// the server binary and test sources.
@@ -81,7 +121,8 @@ func (env *Env) configure() {
 }
 
 // Parse command line and configuration options and print
-// usage if necessary
+// usage if necessary. Exit in incorrect options or configuration
+// file content.
 func (env *Env) Usage() {
 	env.configure()
 
@@ -108,82 +149,95 @@ Positional arguments:
 	env.patterns = pflag.Args()
 }
 
-// An artefact is anything left by a test or suite while
-// it runs. A keyspace created against a running server,
-// a runnign process, etc.
-// Some artefacts must be removed when shutting down,
-// others are cleared between tests.
-// Artefacts such as server data diretory or log file
-// are removed when a test starts, not when it ends, to
-// be able to inspect them in case of a crash or test failure.
-type Artefact interface {
-	Remove()
-}
-
 // Test lane is a directory on disk containing
 // data of a running server, log files and so on.
 type Lane struct {
 	// Artefacts which must be removed before a test starts
-	pre []Artefact
+	removeBeforeNextSuite []Artefact
 	// Artefacts which must be removed at harness exit
-	post []Artefact
+	removeBeforeExit []Artefact
 	// Lane data directory
 	dir string
+	// Unique lane id, used as a subdirectory within the directory
+	id string
+}
+
+func (lane *Lane) AddExitArtefact(artefact Artefact) {
+	lane.removeBeforeExit = append(lane.removeBeforeExit, artefact)
+}
+
+func (lane *Lane) AddSuiteArtefact(artefact Artefact) {
+	lane.removeBeforeNextSuite = append(lane.removeBeforeNextSuite, artefact)
+}
+
+// Used as server working directory
+func (lane *Lane) Dir() string {
+	return lane.dir
+}
+
+func (lane *Lane) Init(id string, dir string) {
+	// @todo add random characters
+	lane.id = id
+	lane.dir, _ = filepath.Abs(path.Join(dir, id))
+	// Create the directory if it doesn't exist or clear
+	// it if it does
+	if _, err := os.Stat(lane.dir); err != nil && !os.IsNotExist(err) {
+		log.Fatalf("Failed to access temporary directory %s", lane.dir)
+	} else if err == nil {
+		if err := os.RemoveAll(lane.dir); err != nil {
+			log.Fatalf("Failed to remove temporary directory %s", lane.dir)
+		}
+	}
+	if err := os.MkdirAll(lane.dir, 0750); err != nil {
+		log.Fatalf("Failed to create temporary directory %s", lane.dir)
+	}
 }
 
 // Clear the lane beween two test suite invocations
-func (lane *Lane) Clear() {
-	// Clear the "post" artefacts first, they may depend on "pre"
-	// artefacts, e.g. a post artefact is a running server and
-	// a pre artefact is its data directory
-	for _, artefact := range lane.post {
+func (lane *Lane) CleanupBeforeNextSuite() {
+	// Clear the "suite" artefacts first, they may depend on "exit"
+	// artefacts, e.g. a running server may depend on the data in
+	// the data directory
+	for _, artefact := range lane.removeBeforeExit {
 		artefact.Remove()
 	}
 	// Clear the artefacts array, the artefacts are now gone
-	lane.post = nil
+	lane.removeBeforeExit = nil
 
-	for _, artefact := range lane.pre {
+	for _, artefact := range lane.removeBeforeNextSuite {
 		artefact.Remove()
 	}
 	// Clear the artefacts array, the artefacts are now gone
-	lane.pre = nil
+	lane.removeBeforeNextSuite = nil
 }
 
 // Remove all artefacts, such as running servers, on an abnormal exit
-
-func (lane *Lane) Abort() {
-	for _, artefact := range lane.post {
+// Keep the test artefacts for inspection.
+func (lane *Lane) CleanupBeforeExit() {
+	for _, artefact := range lane.removeBeforeExit {
 		artefact.Remove()
 	}
-	lane.post = nil
-}
-
-type TestSuite interface {
-	Setup(*Lane)
-	Run(bool, *Lane)
+	lane.removeBeforeExit = nil
 }
 
 // The main testing harness state
 type Yacht struct {
-	env    Env
-	lane   Lane
+	// Options and configuration settings
+	env Env
+	// Execution environemnt, @todo: have many
+	lane Lane
+	// List of suites to run
 	suites []TestSuite
 }
 
-// Remove artefacts of tests which could still be in flight
-func (yacht *Yacht) TearDown() int {
-	yacht.lane.Abort()
-	return 0
-}
-
 // Kill running servers on SIGINT but leave the data directory
-// intact, available for inspection
+// intact
 func setSignalAction(yacht *Yacht) {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		for sig := range c {
-			yacht.TearDown()
+			yacht.lane.CleanupBeforeExit()
 			log.Fatalf("Got signal %v, exiting", sig)
 		}
 	}()
@@ -192,53 +246,100 @@ func setSignalAction(yacht *Yacht) {
 func (yacht *Yacht) PrintSummary() {
 }
 
-func (yacht *Yacht) FindSuites() {
-	files, err := filepath.Glob(yacht.env.srcdir)
+// Go over files in srcdir and look up suite.yaml/json
+// in everything that looks like a dir. If there is a suite
+// configuration file and it has suite type that we recocgnize,
+// add it as a suite to the list of suite.
+// Creating a suite object will put all files in the suite
+// directory to the suite inventory
+func (yacht *Yacht) findSuites() {
+
+	fmt.Printf("Looking for suites at %s", yacht.env.srcdir)
+	files, err := filepath.Glob(path.Join(yacht.env.srcdir, "*"))
 	if err != nil {
 		log.Fatalf("Failed to find suites in %s: %v", yacht.env.srcdir, err)
 	}
-	for _, file := range files {
-		st, err := os.Stat(file)
-		if err != nil || st.IsDir() == false {
+	for _, path := range files {
+		st, err := os.Stat(path)
+		if err != nil {
+			// @todo: add warning color
+			log.Printf("Skipping broken suite %s: %v", path, err)
+			continue
+		}
+		// Skip non-directories, it's OK to have other files in srcdir
+		if st.IsDir() == false {
 			continue
 		}
 		suite_config := viper.New()
 		suite_config.SetConfigName("suite")
-		suite_config.AddConfigPath(file)
-		// Every suite.yaml must have a suite type in it.
+		suite_config.AddConfigPath(path)
+		// Every suite.yaml config must have a suite type and an
+		// optional description.
 		type BasicSuiteConfiguration struct {
-			Type string
+			Type        string
+			Description string
 		}
 		// Skip files which can not be read
 		if err := viper.ReadInConfig(); err == nil {
 			var cfg BasicSuiteConfiguration
 			if err := viper.Unmarshal(&cfg); err != nil {
-				log.Printf("Skipping broken suite %s: %v", file, err)
+				// @todo: add warning color
+				log.Printf("Failed to read suite configuration at %s: %v", path, err)
+				continue
 			}
-
-			//			yacht.suites.add(TestSuiteNew(cfg.Type, file))
+			if cfg.Type != "cql" {
+				// @todo: add warning color
+				log.Printf("Skipping unknown suite type %v at %s",
+					cfg.Type, path)
+				continue
+			}
+			suite := cql_test_suite{
+				description: cfg.Description,
+			}
+			if err := suite.FindTests(path, yacht.env.patterns); err != nil {
+				// @todo add warning color
+				log.Printf("Failed to initialize a suite at %s: %v", path, err)
+				continue
+			}
+			// Only append the siute if it is not empty
+			if suite.IsEmpty() == false {
+				yacht.suites = append(yacht.suites, suite)
+			}
 		}
+	}
+	if len(yacht.suites) == 0 {
+		fmt.Printf(" ...found no matching tests\n")
+	} else {
+		fmt.Printf("\n")
 	}
 }
 
-func (yacht *Yacht) Run() {
+func (yacht *Yacht) Run() int {
 
-	yacht.FindSuites()
+	yacht.lane.Init("1", yacht.env.vardir)
+	var rc int = 0
+
+	yacht.findSuites()
 
 	for _, suite := range yacht.suites {
 		// Clear the lane between test suites
 		// Note, it's done before the suite is started,
-		// not fater, to preserve important artefacts
+		// not after, to preserve important artefacts
 		// between runs
-		yacht.lane.Clear()
+		yacht.lane.CleanupBeforeNextSuite()
 		// @todo: multiple lanes to run tests in parallel
-		suite.Setup(&yacht.lane)
-		suite.Run(yacht.env.force, &yacht.lane)
+		suite.PrepareLane(&yacht.lane)
+		if err := suite.Run(yacht.env.force); err != nil {
+
+			rc = 1
+		}
 		//		if err != nil && env.force == false {
 		//			break
 		//		}
 	}
+	// if all OK clear the lane
 	yacht.PrintSummary()
+	return rc
 }
 
 func main() {
@@ -250,7 +351,153 @@ func main() {
 		env: env,
 	}
 	setSignalAction(&yacht)
-	yacht.Run()
+	defer yacht.lane.CleanupBeforeExit()
+	os.Exit(yacht.Run())
+}
 
-	os.Exit(yacht.TearDown())
+// Implementation
+
+type cql_connection struct {
+	session *gocql.Session
+}
+
+func (c cql_connection) Execute(query string) (string, error) {
+	// @todo handle errors and serialize results
+	c.session.Query(query).Exec()
+	return query, nil
+}
+
+func (c cql_connection) Close() {
+	c.session.Close()
+}
+
+// A pre-installed CQL server to which we connect via a URI
+type cql_server_uri struct {
+	uri     string
+	cluster *gocql.ClusterConfig
+}
+
+// Destroy yacht keyspace when done
+type cql_server_uri_artefact struct {
+	session *gocql.Session
+}
+
+func (a cql_server_uri_artefact) Remove() {
+	a.session.Query("DROP KEYSPACE IF EXISTS yacht").Exec()
+}
+
+func (server cql_server_uri) Start(lane *Lane) error {
+	server.cluster = gocql.NewCluster(server.uri)
+	// Create an administrative session to prepare
+	// administrative server for testing
+	session, err := server.cluster.CreateSession()
+	if err != nil {
+		return err
+	}
+	artefact := cql_server_uri_artefact{session: session}
+	// Cleanup before running the suit
+	artefact.Remove()
+	// Create a keyspace for testing
+	err = session.Query(`CREATE KEYSPACE IF NOT EXISTS yacht WITH REPLICATION =
+{ 'class': 'NetworkTopologyStrategy' } AND DURABLE_WRITES=true`).Exec()
+	if err != nil {
+		return err
+	}
+	server.cluster.Keyspace = "yacht"
+	lane.AddSuiteArtefact(&artefact)
+	return nil
+}
+
+func (server cql_server_uri) Connect() (Connection, error) {
+	session, err := server.cluster.CreateSession()
+	if err != nil {
+		return nil, err
+	}
+	return cql_connection{session: session}, nil
+}
+
+// A suite with CQL tests
+type cql_test_suite struct {
+	description string
+	path        string
+	name        string
+	tests       []cql_test_file
+	server      Server
+}
+
+func (suite cql_test_suite) FindTests(suite_path string, patterns []string) error {
+	suite.path = suite_path
+	suite.name = path.Base(suite.path)
+
+	files, err := filepath.Glob(path.Join(suite.path, "*.test.cql"))
+	if err != nil {
+		return err
+	}
+	// @todo: say nothing if there are no tests
+	fmt.Printf("Collecting tests in '%12s' (Found %4d tests): %20s\n",
+		suite.name, len(files), suite.description)
+	for _, file := range files {
+		// @todo: filter by pattern here
+		suite.tests = append(suite.tests, cql_test_file{path: file, name: path.Base(file)})
+	}
+	return nil
+}
+
+func (suite cql_test_suite) IsEmpty() bool {
+	return len(suite.tests) == 0
+}
+
+func (suite cql_test_suite) PrepareLane(lane *Lane) {
+	suite.server = cql_server_uri{uri: "127.0.0.1"}
+	suite.server.Start(lane)
+}
+
+func (suite cql_test_suite) Run(force bool) error {
+	c, err := suite.server.Connect()
+	if err != nil {
+		// 'force' affects .result/reject mismatch,
+		// but not harness failures
+		return err
+	}
+	defer c.Close()
+	for _, test := range suite.tests {
+		err := test.Run(force, c)
+		if err != nil {
+			// @todo nice progress report
+			// 'force' doesn't affect internal errors
+			return err
+		}
+		// @todo nice output, nice progress report
+		fmt.Println("OK")
+	}
+	return nil
+}
+
+type cql_test_file struct {
+	name string
+	path string
+}
+
+// Open a file and read it line-by-line, splitting into test cases.
+func (test cql_test_file) Run(force bool, c Connection) error {
+
+	// Open input file
+	file, err := os.Open(test.path)
+	if err != nil {
+		return err
+	}
+
+	stream := bufio.NewScanner(file)
+
+	// @todo: fail if found no tests
+	for stream.Scan() {
+		line := stream.Text()
+		if _, err := c.Execute(line); err != nil {
+			// @todo: access denied, lost connection
+			// should not trigger test failure with 'force'
+			return err
+		}
+		// append output to the output file
+	}
+	return nil
 }
